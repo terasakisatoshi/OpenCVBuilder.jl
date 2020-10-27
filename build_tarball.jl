c165589b3cbf7fe1febb0ecb08f1e4149be15eab
SatoshiTerasaki <terasakisatoshi.math@gmail.com>
2020-10-28 01:49:07 +0900

    up

--- build_tarball.jl
+++ build_tarball.jl
@@ -3,7 +3,7 @@
 using BinaryBuilder, Pkg
 
 name = "OpenCV"
-version = v"0.2"
+version = v"0.2.2"
 
 # Collection of sources required to complete build
 sources = [
@@ -26,6 +26,7 @@ cd build
 cmake -DCMAKE_FIND_ROOT_PATH=$prefix \
       -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
       $macos_extra_flags -DCMAKE_BUILD_TYPE=Release \
+      WITH_QT=ON \
       ../opencv/
 VERBOSE=ON cmake --build . --config Release --target install -- -j${nproc}
 cd ..
@@ -36,13 +37,13 @@ install_license opencv/LICENSE
 # platforms are passed in on the command line
 platforms = [
     #FreeBSD(:x86_64; compiler_abi=CompilerABI(cxxstring_abi=:cxx11)), <- fails
-    Linux(:armv7l; libc=:glibc, compiler_abi=CompilerABI(cxxstring_abi=:cxx11)),
-    Linux(:aarch64; libc=:glibc, compiler_abi=CompilerABI(cxxstring_abi=:cxx11)),
+    #Linux(:armv7l; libc=:glibc, compiler_abi=CompilerABI(cxxstring_abi=:cxx11)),
+    #Linux(:aarch64; libc=:glibc, compiler_abi=CompilerABI(cxxstring_abi=:cxx11)),
     Linux(:x86_64; libc=:glibc, compiler_abi=CompilerABI(cxxstring_abi=:cxx11)),
-    Linux(:i686; libc=:glibc, compiler_abi=CompilerABI(cxxstring_abi=:cxx11)),
-    MacOS(:x86_64; compiler_abi=CompilerABI(cxxstring_abi=:cxx11)),
-    Windows(:x86_64; compiler_abi=CompilerABI(cxxstring_abi=:cxx11)),
-    Windows(:i686; compiler_abi=CompilerABI(cxxstring_abi=:cxx11)),
+    #Linux(:i686; libc=:glibc, compiler_abi=CompilerABI(cxxstring_abi=:cxx11)),
+    #MacOS(:x86_64; compiler_abi=CompilerABI(cxxstring_abi=:cxx11)),
+    #Windows(:x86_64; compiler_abi=CompilerABI(cxxstring_abi=:cxx11)),
+    #Windows(:i686; compiler_abi=CompilerABI(cxxstring_abi=:cxx11)),
 ]
                                 
 # The products that we will ensure are always built
@@ -65,7 +66,9 @@ products = [
 ]
 
 # Dependencies that must be installed before this package can be built
-dependencies = []
+dependencies = [
+    BuildDependency("Qt_jll")
+]
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version = v"7")
