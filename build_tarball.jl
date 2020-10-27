0ebf8fdbc9c4cb7198db6eff39607947acf10075
SatoshiTerasaki <terasakisatoshi.math@gmail.com>
2020-10-28 02:51:27 +0900

    update

--- build_tarball.jl
+++ build_tarball.jl
@@ -3,7 +3,7 @@
 using BinaryBuilder, Pkg
 
 name = "OpenCV"
-version = v"0.2.3"
+version = v"0.2.4"
 
 # Collection of sources required to complete build
 sources = [
@@ -40,10 +40,10 @@ platforms = [
     #Linux(:armv7l; libc=:glibc, compiler_abi=CompilerABI(cxxstring_abi=:cxx11)),
     #Linux(:aarch64; libc=:glibc, compiler_abi=CompilerABI(cxxstring_abi=:cxx11)),
     Linux(:x86_64; libc=:glibc, compiler_abi=CompilerABI(cxxstring_abi=:cxx11)),
-    #Linux(:i686; libc=:glibc, compiler_abi=CompilerABI(cxxstring_abi=:cxx11)),
-    #MacOS(:x86_64; compiler_abi=CompilerABI(cxxstring_abi=:cxx11)),
-    #Windows(:x86_64; compiler_abi=CompilerABI(cxxstring_abi=:cxx11)),
-    #Windows(:i686; compiler_abi=CompilerABI(cxxstring_abi=:cxx11)),
+    Linux(:i686; libc=:glibc, compiler_abi=CompilerABI(cxxstring_abi=:cxx11)),
+    MacOS(:x86_64; compiler_abi=CompilerABI(cxxstring_abi=:cxx11)),
+    Windows(:x86_64; compiler_abi=CompilerABI(cxxstring_abi=:cxx11)),
+    Windows(:i686; compiler_abi=CompilerABI(cxxstring_abi=:cxx11)),
 ]
                                 
 # The products that we will ensure are always built
