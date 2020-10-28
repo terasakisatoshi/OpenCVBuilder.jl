07dd900508a693b9e8a7b9f033520920c5c34389
SatoshiTerasaki <terasakisatoshi.math@gmail.com>
2020-10-28 17:18:31 +0900

    modify name

--- qt/build_tarball.jl
+++ qt/build_tarball.jl
@@ -2,7 +2,7 @@
 # `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder, Pkg
 
-name = "OpenCV"
+name = "OpenCVQt"
 version = v"0.3.0"
 
 # Collection of sources required to complete build
