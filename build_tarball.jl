7242044eb7040750ae13500113c44e1892649eeb
SatoshiTerasaki <terasakisatoshi.math@gmail.com>
2020-10-28 11:34:21 +0900

    update

--- build_tarball.jl
+++ build_tarball.jl
@@ -19,6 +19,10 @@ if [[ $target == *"apple-darwin"* ]]; then
   macos_extra_flags="-DCMAKE_CXX_COMPILER_ID=AppleClang -DCMAKE_CXX_COMPILER_VERSION=10.0.0 -DCMAKE_CXX_STANDARD_COMPUTED_DEFAULT=11"
 fi
 
+if [[ $target == "x86_64-linux-gnu"* ]]; then
+  gui_flags="-DWITH_QT=ON"
+fi
+
 Julia_PREFIX=$prefix
 
 mkdir build
@@ -26,7 +30,7 @@ cd build
 cmake -DCMAKE_FIND_ROOT_PATH=$prefix \
       -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
       $macos_extra_flags -DCMAKE_BUILD_TYPE=Release \
-      -DWITH_QT=ON \
+      $gui_flags \
       ../opencv/
 VERBOSE=ON cmake --build . --config Release --target install -- -j${nproc}
 cd ..
@@ -40,10 +44,10 @@ platforms = [
     #Linux(:armv7l; libc=:glibc, compiler_abi=CompilerABI(cxxstring_abi=:cxx11)),
     #Linux(:aarch64; libc=:glibc, compiler_abi=CompilerABI(cxxstring_abi=:cxx11)),
     Linux(:x86_64; libc=:glibc, compiler_abi=CompilerABI(cxxstring_abi=:cxx11)),
-    Linux(:i686; libc=:glibc, compiler_abi=CompilerABI(cxxstring_abi=:cxx11)),
-    MacOS(:x86_64; compiler_abi=CompilerABI(cxxstring_abi=:cxx11)),
-    Windows(:x86_64; compiler_abi=CompilerABI(cxxstring_abi=:cxx11)),
-    Windows(:i686; compiler_abi=CompilerABI(cxxstring_abi=:cxx11)),
+    #Linux(:i686; libc=:glibc, compiler_abi=CompilerABI(cxxstring_abi=:cxx11)),
+    #MacOS(:x86_64; compiler_abi=CompilerABI(cxxstring_abi=:cxx11)),
+    #Windows(:x86_64; compiler_abi=CompilerABI(cxxstring_abi=:cxx11)),
+    #Windows(:i686; compiler_abi=CompilerABI(cxxstring_abi=:cxx11)),
 ]
                                 
 # The products that we will ensure are always built
@@ -68,7 +72,7 @@ products = [
 # Dependencies that must be installed before this package can be built
 dependencies = [
     Dependency("Qt_jll"),
-    Dependency("GTK3_jll"),
+    BuildDependency("GTK3_jll"),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
